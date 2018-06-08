ab3fdcb9e4ab0695beb3264a4d7b18b9e2b557a0
Sheila Wittman <szandora789@gmail.com>
2018-06-08 21:32:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,20 @@
 
 katz_deli = []
 
-def line(number)
+def line(katz_deli, number)
   number = katz_deli += 1
   number > 0 ? "Place in line is #{number}" : "The line is currently empty."
 end
+
+
+def take_a_number(katz_deli, name)
+  number = katz_deli += 1
+  puts "#{name} number"
+end
+
+
+def now_serving(katz_deli)
+
+  puts "#{name}"
+  katz_deli.first
+  
